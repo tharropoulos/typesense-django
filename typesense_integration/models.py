11432cfd60df069ca21ab0a06658cec62365ba14
Fanis Tharropoulos <ftharropoulos@gmail.com>
2024-07-12 11:07:32 +0300

    feat(collection): add model and client validation

--- typesense_integration/models.py
+++ typesense_integration/models.py
@@ -1,1 +1,32 @@
-# Create your models here.
+from django.db import models
+from typesense import Client
+
+class TypesenseCollection:
+    """Class for mapping a Django model to a Typesense collection."""
+
+    def __init__(
+        self,
+        *,
+        client: Client,
+        model: models.Model,
+    ) -> None:
+        """
+        Initializes a new instance of TypesenseCollection.
+
+        :param client: An instance of Client to interact with the Typesense API.
+        :param model: The Django model that this instance is associated with.
+        """
+        self.model = model
+        self.client = client
+        self._validate_client_and_model()
+
+    def _validate_client_and_model(self):
+        if not isinstance(self.client, Client):
+            raise typesense_exceptions.ConfigError(
+                'Client must be an instance of Typesense Client.',
+            )
+        if not issubclass(self.model, models.Model):
+            raise typesense_exceptions.ConfigError(
+                'Model must be an instance of the default models.Model class.',
+            )
+
