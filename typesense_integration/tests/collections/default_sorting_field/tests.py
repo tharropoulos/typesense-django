753ca48514211f8f609bb8b6cfd908c0ed378eb5
Fanis Tharropoulos <ftharropoulos@gmail.com>
2024-07-22 15:01:55 +0300

    test(collection): add more test cases for default sorting fields
    adds more test cases to check for if they're listed as sorted fields and
    for string types

--- typesense_integration/tests/collections/default_sorting_field/tests.py
+++ typesense_integration/tests/collections/default_sorting_field/tests.py
@@ -3,7 +3,12 @@ from __future__ import annotations
 from typesense import exceptions as typesense_exceptions
 
 from typesense_integration.models import TypesenseCollection
-from typesense_integration.tests.collections.models import Book, Reference
+from typesense_integration.tests.collections.models import (
+    Book,
+    MultipleSortableFields,
+    OptionalFields,
+    Reference,
+)
 from typesense_integration.tests.collections.tests import TypesenseCollectionTestCase
 
 
@@ -23,6 +28,24 @@ class DefaultSortingFieldTests(TypesenseCollectionTestCase):
         self.assertEqual(
             collection.default_sorting_field,
             Reference._meta.get_field('number'),
+        )
+
+    def test_default_sorting_field_string(self) -> None:
+        """Test that it can handle default sorting fields if they're strings."""
+        mock_client_instance = self.mock_client()
+
+        collection = TypesenseCollection(
+            client=mock_client_instance,
+            model=MultipleSortableFields,
+            sorting_fields={
+                MultipleSortableFields._meta.get_field('name'),
+            },
+            default_sorting_field=MultipleSortableFields._meta.get_field('name'),
+        )
+
+        self.assertEqual(
+            collection.default_sorting_field,
+            MultipleSortableFields._meta.get_field('name'),
         )
 
     def test_raises_default_sorting_field_not_number(self) -> None:
@@ -97,3 +120,32 @@ class DefaultSortingFieldTests(TypesenseCollectionTestCase):
                 collection.default_sorting_field,
                 Reference._meta.get_field('number'),
             )
+
+    def test_raises_default_sorting_field_not_sortable(self) -> None:
+        """Test all field sortable."""
+        mock_client_instance = self.mock_client()
+
+        with self.assertRaises(typesense_exceptions.RequestMalformed):
+            TypesenseCollection(
+                client=mock_client_instance,
+                model=MultipleSortableFields,
+                sorting_fields={
+                    MultipleSortableFields._meta.get_field('name'),
+                },
+                default_sorting_field=MultipleSortableFields._meta.get_field('second'),
+            )
+
+    def test_raises_default_sorting_field_optional(self) -> None:
+        """Test all field sortable."""
+        mock_client_instance = self.mock_client()
+
+        with self.assertRaises(typesense_exceptions.RequestMalformed):
+            collection = TypesenseCollection(
+                client=mock_client_instance,
+                model=OptionalFields,
+                sorting_fields={
+                    OptionalFields._meta.get_field('optional'),
+                },
+                default_sorting_field=OptionalFields._meta.get_field('optional'),
+            )
+            collection.create()
